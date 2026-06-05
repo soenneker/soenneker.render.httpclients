6cfc3e35ee65d515ed617ca7a91cf7a5fd30a668
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:40 -0500

    xml commenting

--- src/Soenneker.Render.HttpClients/RenderOpenApiHttpClient.cs
+++ src/Soenneker.Render.HttpClients/RenderOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class RenderOpenApiHttpClient : IRenderOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(RenderOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(RenderOpenApiHttpClient));
